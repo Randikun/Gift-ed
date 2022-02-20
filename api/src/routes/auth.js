0176c391a4b59fc55a89b6047a944b7639eb93f2
randikun <guadafantini@gmail.com>
2022-02-20 20:32:21 -0300

    logout completado con refreshtoken

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const { verifyTokenAndAuthorization } = require("./verifyToken");
+
 
 //REGISTER
 router.post("/register", async (req, res) => {
@@ -23,34 +25,78 @@ router.post("/register", async (req, res) => {
 }
 });
 
-//LOGIN
 
-router.post("/login", async (req, res) => {
-    if (req.body.email && req.body.password){
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    !user && res.status(404).send("user not found");
-
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
-    !validPassword && res.status(400).send("wrong password")
-    
-    const accessToken = jwt.sign({
+const generateAccessToken = (user) =>{
+    return  jwt.sign({
         id: user._id,
         isAdmin: user.isAdmin
     }, process.env.JWT_SECRET, {
         expiresIn: "2d"
     })
+}
 
-    res.status(200).json({user, accessToken})
-  } catch (err) {
-   res.status(500).json(err)
-  }}
-  else res.send("please enter an email and a valid password")
-});
+const generateRefreshToken = (user) =>{
+    return jwt.sign({
+        id: user._id,
+        isAdmin: user.isAdmin
+    }, process.env.JWT_REFRESH_SECRET)
+}
 
-//LOGOUT
+const refreshTokens = [];
 
+router.post("/refresh", (req, res) =>{
+    // take users refresh token
+        let reftoken = req.body.token
+        //send error if token isnt valid
+        if(!reftoken) return res.status(401).json("not authenticated")
+        //if it ok create new access token, and refresh token and send it to user
+        if(!refreshTokens.includes(reftoken)){
+            return res.status(403).json("invalid token")
+        }
+        
+        jwt.verify(reftoken, process.env.JWT_REFRESH_SECRET, (err, user)=>{
+            err && console.log(err)
+            
+            refreshTokens = refreshTokens.filter(token=> token !== reftoken)
+            
+            const newAccessToken = generateAccessToken(user)
+            const newRefreshToken = generateRefreshToken(user)
+            
+            refreshTokens.push(newRefreshToken)
+            
+            res.json({accessToken: newAccessToken, refreshToken: newRefreshToken})
+        })
+    })
+    //LOGIN
+    
+    router.post("/login", async (req, res) => {
+        if (req.body.email && req.body.password){
+            try {
+                const user = await User.findOne({ email: req.body.email });
+                !user && res.status(404).send("user not found");
+                
+                const validPassword = await bcrypt.compare(req.body.password, user.password)
+                !validPassword && res.status(400).send("wrong password")
+                
+                const accessToken = generateAccessToken(user)
+                const refreshToken = generateRefreshToken(user)
+                
+                refreshTokens.push(refreshToken)
+                
+                res.status(200).json({user, accessToken, refreshToken})
+            } catch (err) {
+                res.status(500).json(err)
+            }}
+            else res.send("please enter an email and a valid password")
+        });
+    
+    //LOGOUT
 
-
-
-module.exports = router;
+    router.post("/logout", verifyTokenAndAuthorization, (req,res) => {
+        const refreshToken = res.body.token;
+        refreshTokens = refreshTokens.filter(token => token !== refreshToken)
+        res.json("succesfull logout")
+    })
+    
+    module.exports = router;
+    
